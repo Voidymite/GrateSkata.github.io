9dad3875a859824a7610ddd060b5664e4b95752d
TSG <49595354+AyeTSG@users.noreply.github.com>
2021-03-02 16:21:21 -0500

    add music player

---
+++ js/music_helper.js
@@ -0,0 +1,41 @@
+// helper song class
+class Song {
+    constructor(details, url) {
+        this.details = details;
+        this.url = url;
+    }
+}
+
+// songs list
+var songs = [
+    new Song("Basset - Monster", "https://raw.githubusercontent.com/ninjamuffin99/Funkin/master/assets/songs/monster/Inst.mp3"),
+    new Song("Kawai Sprite - Test", "https://raw.githubusercontent.com/ninjamuffin99/Funkin/master/assets/songs/test/Inst.mp3")
+]
+
+// choose a song
+songToUse = songs[Math.floor(Math.random() * songs.length)];
+
+// wait for page DOM to load
+window.addEventListener("load", function() {
+    // create the player and it's info blurb
+    var playerDiv = document.createElement("div");
+    var playerBrk = document.createElement("br");
+    var playerInfo = document.createElement("span");
+    var player = document.createElement("audio");
+
+    // set the player and blurb properties
+    playerDiv.classList.add("player_box");
+    playerInfo.innerText = "🎵: " + songToUse.details;
+    playerInfo.classList.add("music_info");
+    player.setAttribute("controls", "");
+    player.setAttribute("loop", "");
+    player.classList.add("music_player");
+    player.src = songToUse.url;
+    player.volume = 0.2;
+
+    // add the player and blurb
+    playerDiv.appendChild(playerInfo);
+    playerDiv.appendChild(playerBrk);
+    playerDiv.appendChild(player);
+    document.body.appendChild(playerDiv);
+});
