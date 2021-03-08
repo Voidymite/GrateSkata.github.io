21e2ab2319a5359df1f4af5e378281f3623a5ecc
TSG <49595354+AyeTSG@users.noreply.github.com>
2021-03-08 13:28:55 -0500

    mr burns likes to credit original sources!

--- js/easter_egg_handler.js
+++ js/easter_egg_handler.js
@@ -8,7 +8,7 @@ class RedirectEasterEgg {
 var redirect_easter_eggs = [
     new RedirectEasterEgg("c,r,o,u,t,o,n", "https://crouton.net"),
     new RedirectEasterEgg("m,c,r,i,b", "https://twitter.com/PointyyESM/status/1364715306611073032"),
-    new RedirectEasterEgg("m,r,b,u,r,n,s", "https://twitter.com/i/status/1368639794666934277"),
+    new RedirectEasterEgg("m,r,b,u,r,n,s", "https://www.youtube.com/watch?v=RtJ9YB-wTqw"),
     new RedirectEasterEgg("s,e,n,p,a,i", "https://ayetsg.github.io/files/senpai_is_real.mp4")
 ]
 
