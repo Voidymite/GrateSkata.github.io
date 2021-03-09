e0eb421ab6e7fc9ae1156d5bae3245624cab57a0
TSG <49595354+AyeTSG@users.noreply.github.com>
2021-03-08 19:22:03 -0500

    sus

--- js/easter_egg_handler.js
+++ js/easter_egg_handler.js
@@ -9,7 +9,8 @@ var redirect_easter_eggs = [
     new RedirectEasterEgg("c,r,o,u,t,o,n", "https://crouton.net"),
     new RedirectEasterEgg("m,c,r,i,b", "https://twitter.com/PointyyESM/status/1364715306611073032"),
     new RedirectEasterEgg("m,r,b,u,r,n,s", "https://www.youtube.com/watch?v=RtJ9YB-wTqw"),
-    new RedirectEasterEgg("s,e,n,p,a,i", "https://ayetsg.github.io/files/senpai_is_real.mp4")
+    new RedirectEasterEgg("s,e,n,p,a,i", "https://ayetsg.github.io/files/senpai_is_real.mp4"),
+    new RedirectEasterEgg("s,u,s", "https://whentheimposteriss.us")
 ]
 
 var egg = new Egg();
