81409393cfeb2bd6c006ad8135e40d45d4bd5903
TSG <49595354+AyeTSG@users.noreply.github.com>
2021-03-10 11:24:56 -0500

    prevent oaoooa layering

--- js/easter_egg_handler.js
+++ js/easter_egg_handler.js
@@ -16,41 +16,47 @@ class FunctionEasterEgg{
     }
 };
 
+var oaoooa_active = false;
+
 var function_easter_eggs = [
     // == oaoooa ==
     new FunctionEasterEgg("o,a,o,o,o,a", function() {
-        // setup shit
-        var ov_img = document.createElement("img");
-        var boyfriend_img_path = "https://ayetsg.github.io/img/boyfriend.png";
-        var dad_img_path = "https://ayetsg.github.io/img/dad.png";
-        var oaoooa_dad_audio = new Audio("https://ayetsg.github.io/files/oaoooa_dad.mp3");
-        var oaoooa_bf_audio = new Audio("https://ayetsg.github.io/files/oaoooa_bf.mp3");
-        var overlay_div = document.createElement("div");
+        if (oaoooa_active === false) {
+            oaoooa_active = true;
 
-        var bf_active = false;
+            // setup shit
+            var ov_img = document.createElement("img");
+            var boyfriend_img_path = "https://ayetsg.github.io/img/boyfriend.png";
+            var dad_img_path = "https://ayetsg.github.io/img/dad.png";
+            var oaoooa_dad_audio = new Audio("https://ayetsg.github.io/files/oaoooa_dad.mp3");
+            var oaoooa_bf_audio = new Audio("https://ayetsg.github.io/files/oaoooa_bf.mp3");
+            var overlay_div = document.createElement("div");
+            var bf_active = false;
 
-        overlay_div.style = "z-index: 2; position: absolute; top: 50%; left: 50%; text-align: center; top: 50%; left: 50%; transform: translate(-50%, -50%) scale(75%, 75%);"
+            overlay_div.style = "z-index: 2; position: absolute; top: 50%; left: 50%; text-align: center; top: 50%; left: 50%; transform: translate(-50%, -50%) scale(75%, 75%);"
 
-        overlay_div.id = "overlay";
-        ov_img.src = dad_img_path;
-        overlay_div.appendChild(ov_img);
-        document.body.appendChild(overlay_div);
+            overlay_div.id = "overlay";
+            ov_img.src = dad_img_path;
+            overlay_div.appendChild(ov_img);
+            document.body.appendChild(overlay_div);
 
-        // play the audio
-        oaoooa_dad_audio.volume = 0.2;
-        oaoooa_bf_audio.volume = 0.2;
-        oaoooa_dad_audio.play();
+            // play the audio
+            oaoooa_dad_audio.volume = 0.2;
+            oaoooa_bf_audio.volume = 0.2;
+            oaoooa_dad_audio.play();
 
-        // when the audio ends, remove the overlay
-        oaoooa_dad_audio.onended = function() {
-            ov_img.src = boyfriend_img_path;
-            bf_active = true;
-            
-            oaoooa_bf_audio.play();
-        }
+            // when the audio ends, remove the overlay
+            oaoooa_dad_audio.onended = function() {
+                ov_img.src = boyfriend_img_path;
+                bf_active = true;
+                
+                oaoooa_bf_audio.play();
+            }
 
-        oaoooa_bf_audio.onended = function() {
-            overlay_div.remove();
+            oaoooa_bf_audio.onended = function() {
+                overlay_div.remove();
+                oaoooa_active = false;
+            }
         }
     })
 ]
