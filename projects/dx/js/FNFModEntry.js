259550d1c1163978a4b8edd4cb663b5bcba7f351
TSG <49595354+AyeTSG@users.noreply.github.com>
2021-05-10 08:31:04 -0400

    dx wip

---
+++ projects/dx/js/FNFModEntry.js
@@ -0,0 +1,12 @@
+class FNFMod {
+    constructor(name, thumbnail_url, page_url) {
+        this.name = name;
+        this.thumbnail_url = thumbnail_url;
+        this.page_url = page_url;
+    }
+}
+
+_FNF_MODS = [
+    // FNF Mod: Small Things
+    new FNFMod("FNF Small Things", "https://ayetsg.github.io/projects/dx/img/mods/st/thumbnail.png", null)
+]
