5c3b47ebb6ab6cff5f4c516a8b74d3c4b1867e33
TSG <49595354+AyeTSG@users.noreply.github.com>
2021-05-28 16:55:41 -0400

    cyrix dl update

--- projects/dx/js/FNFModMetadata.js
+++ projects/dx/js/FNFModMetadata.js
@@ -42,8 +42,8 @@ _FNF_MODS = [
         [
             new DownloadLink("GameBanana", "https://gamebanana.com/mods/287411"),
             new DownloadLink("GitHub Source", "https://github.com/AyeTSG/Funkin_VsCyrix"),
-            new DownloadLink("Google Drive", "https://drive.google.com/file/d/1lDx4TmOYTtWfkrRQ6NMbOkw8lApCgR2B/view?usp=sharing"),
-            new DownloadLink("MEGA", "https://mega.nz/file/zW5FDIBS#aiUdgKTsI9RYcYXa8L3WZuOcQWuEvjandTm0uZwT8rg")
+            new DownloadLink("Google Drive", "https://drive.google.com/file/d/1Qr6ul_aJrazTXybNYwcG8KCzgTbu4OPf/view?usp=sharing"),
+            new DownloadLink("MEGA", "https://mega.nz/file/aCJHBCpK#dwhxSDzQtKfhX-3sXd3AjQmFHWn597EWoAiaYQ-fvWs")
         ],
         "vs_cyrix"
     ),
