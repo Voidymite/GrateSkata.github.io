581b9536911a5062907aec9fd220412611943b30
AyeTSG <49595354+AyeTSG@users.noreply.github.com>
2021-09-30 00:23:10 -0400

    gaster speaker config

---
+++ projects/17/js/speaker_configs.js
@@ -0,0 +1,28 @@
+// File: speaker_configs.js
+// Author: AyeTSG
+// Purpose: Provide different "speakers" for the undertale-styled typer
+
+var speaker_configs = {
+    // Gaster
+    "gaster": {
+        // What font should the speaker use?
+        "font": "wingdings_bitmapped_images",
+
+        // Should the speaker show their icon?
+        "uses_speaker_icon": false,
+
+        // What icon should the speaker use?
+        "speak_icon": null,
+
+        // What sounds should be played when a character is typed?
+        "speak_sounds": [
+            "sounds/snd_wngdng1.wav",
+            "sounds/snd_wngdng2.wav",
+            "sounds/snd_wngdng3.wav",
+            "sounds/snd_wngdng4.wav",
+            "sounds/snd_wngdng5.wav",
+            "sounds/snd_wngdng6.wav",
+            "sounds/snd_wngdng7.wav"
+        ]
+    }
+}
