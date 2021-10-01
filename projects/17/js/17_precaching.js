4bbc9b452494ed80532aadeaec0e02cdb32fd112
AyeTSG <49595354+AyeTSG@users.noreply.github.com>
2021-10-01 02:30:41 -0400

    wip shit, i needa go to bed now fr

--- projects/17/js/17_precaching.js
+++ projects/17/js/17_precaching.js
@@ -7,80 +7,91 @@
 // Purpose: List of image assets to precache
 var _IMAGES_TO_PRECACHE = [
     "spr_mysteryman/0.png",
-    "spr_mysteryman/1.png",
-    "wingdings_font_images/A.png",
-    "wingdings_font_images/B.png",
-    "wingdings_font_images/C.png",
-    "wingdings_font_images/D.png",
-    "wingdings_font_images/dot.png",
-    "wingdings_font_images/E.png",
-    "wingdings_font_images/G.png",
-    "wingdings_font_images/H.png",
-    "wingdings_font_images/I.png",
-    "wingdings_font_images/K.png",
-    "wingdings_font_images/M.png",
-    "wingdings_font_images/N.png",
-    "wingdings_font_images/O.png",
-    "wingdings_font_images/P.png",
-    "wingdings_font_images/question_mark.png",
-    "wingdings_font_images/R.png",
-    "wingdings_font_images/S.png",
-    "wingdings_font_images/space.png",
-    "wingdings_font_images/T.png",
-    "wingdings_font_images/U.png",
-    "wingdings_font_images/V.png",
-    "wingdings_font_images/W.png",
-    "wingdings_font_images/X.png",
-    "wingdings_font_images/Y.png"
+    "spr_mysteryman/1.png"
 ];
 
 // Purpose: List of sound assets to precache
 var _SOUNDS_TO_PRECACHE = [
     "music/mus_smile.ogg",
-    "sounds/snd_mysterygo.ogg",
-    "sounds/snd_wngdng1.wav",
-    "sounds/snd_wngdng2.wav",
-    "sounds/snd_wngdng3.wav",
-    "sounds/snd_wngdng4.wav",
-    "sounds/snd_wngdng5.wav",
-    "sounds/snd_wngdng6.wav",
-    "sounds/snd_wngdng7.wav"
+    "sounds/snd_mysterygo.ogg"
 ]
 
-// Purpose: Precache all of the sounds and music
-async function precacheSounds() {
-    // Loop over all of the sounds
-    for (var i = 0; i < _SOUNDS_TO_PRECACHE.length; i++) {
-        // Create the sound
-        sound = new Howl({
-            src: ['./audio/' + _SOUNDS_TO_PRECACHE[i]],
-            volume: 1.0
-        });
-    }
+var _CHARACTER_LIST = [];
+
+// Purpose: Precaches a sound
+async function precacheSound(in_sound_path) {
+    // Create the sound
+    sound = new Howl({
+        src: ['./audio/' + in_sound_path],
+        volume: 1.0
+    });
 
     return;
 }
 
-// Purpose: Precache all of the images
-async function precacheImages(_callback) {
-    // Loop over all of the images
-    for (var i = 0; i < _IMAGES_TO_PRECACHE.length; i++) {
-        // Create an image
-        img = document.createElement("img");
-        img.src = "./images/" + _IMAGES_TO_PRECACHE[i];
-        img.classList.add("gaster_dingbat");
-        document.getElementById("precache_image_holder").appendChild(img);
-    }
+// Purpose: Precaches an image
+async function precacheImage(in_image_path) {
+    // Create an image
+    img = document.createElement("img");
+    img.src = "./images/" + in_image_path;
+    img.classList.add("precached_image");
+    document.getElementById("precache_image_holder").appendChild(img);
 
     return;
+}
+
+// Purpose: Filter characters
+function filterCharacter(in_character) {
+    if (in_character == " ") {
+        return "space";
+    } else if (in_character == ".") {
+        return "dot";
+    } else if (in_character == "?") {
+        return "question_mark";
+    } else {
+        return in_character;
+    }
 }
 
 // Purpose: Wait for the page to load
 document.addEventListener('DOMContentLoaded', async function() {
-    // Precache assets
-    await precacheSounds();
-    await precacheImages();
+    // Precache pre-defined images
+    for (var i = 0; i < _IMAGES_TO_PRECACHE.length; i++) {
+        precacheImage(_IMAGES_TO_PRECACHE[i]);
+    }
+
+    // Precache pre-defined sounds
+    for (var i = 0; i < _SOUNDS_TO_PRECACHE.length; i++) {
+        precacheSound(_SOUNDS_TO_PRECACHE[i]);
+    }
+
+    // Loop over all of the message entries
+    for (var i = 0; i < Object.keys(messages).length; i++) {
+        current_entry = Object.keys(messages).at(i);
+        current_speaker = messages[current_entry]["speaker"];
+
+        // Loop over all of the subentries
+        for (var j = 0; j < messages[current_entry]["ordered_entries"].length; j++) {
+            current_subentry = messages[current_entry]["ordered_entries"][j];
+
+            // Loop over each letter
+            for (var k = 0; k < current_subentry.length; k++) {
+                current_character = filterCharacter(current_subentry[k]);
+
+                if (!_CHARACTER_LIST[current_speaker + "__" + current_character]) {
+                    _CHARACTER_LIST[current_speaker + "__" + current_character] = 0;
+                }
+
+                // Add it to a character list
+                _CHARACTER_LIST[current_speaker + "__" + current_character] += 1;
+            }
+        }
+    }
+
+    // TODO: Loop through all character list entries
+    // TODO: Split those entries by speaker name and character
+    // TODO: Load the character images based on the speaker config data
 
     // Unlock the precache lock
-    unlockPrecacheLock();
+    //unlockPrecacheLock();
 });
