9b6fa2a508d096f767b82e4e08064a70564eb532
AyeTSG <49595354+AyeTSG@users.noreply.github.com>
2021-10-02 23:56:49 -0400

    create speaker configuration data

---
+++ projects/undertyper/js/speaker_configs.js
@@ -0,0 +1,26 @@
+// File: speaker_configs.js
+// Author: AyeTSG
+// Project: UnderTyper
+// Purpose: Define different "speakers"
+
+/* NOTE: The format used here is a derivative of
+   the speaker_configs.js used in the 17 project. */
+
+var speaker_configs = {
+    "gaster": {
+        "font_data_folder": "wingdings_font_images",
+        "uses_speaker_icon": false,
+        "speak_icon": null,
+        "speak_name": "W.D. Gaster",
+        "speak_sounds": [
+            "sounds/snd_wngdng1.wav",
+            "sounds/snd_wngdng2.wav",
+            "sounds/snd_wngdng3.wav",
+            "sounds/snd_wngdng4.wav",
+            "sounds/snd_wngdng5.wav",
+            "sounds/snd_wngdng6.wav",
+            "sounds/snd_wngdng7.wav"
+        ],
+        "time_type": 150
+    }
+}
